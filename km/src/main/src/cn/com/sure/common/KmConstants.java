f2bd2b017a54a9b0d3aca3c6cd663f52c5a4317a
946082270@qq.com <946082270@qq.com>
2017-05-24 17:11:52 +0800

    1

---
+++ km/src/main/src/cn/com/sure/common/KmConstants.java
@@ -0,0 +1,14 @@
+package cn.com.sure.common;
+
+public class KmConstants {
+	
+	public static final int YES_OR_NO_OPTION_YES = 1;
+	public static final int YES_OR_NO_OPTION_NO = 0;
+	
+	public static final long OPERATION_TYPE_INSERT=1;
+	public static final long OPERATION_TYPE_DELETE=2;
+	public static final long OPERATION_TYPE_UPDATE=3;
+	public static final long OPERATION_TYPE__SELECT=4;
+	
+
+}
